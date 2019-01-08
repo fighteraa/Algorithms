219e288e6cf780e410aa161c8bd4ac87dd1614cd
Andrey <fighteraa@gmail.com>
2019-01-08 15:45:14 +0300

    Lesson-3

--- task-1.py
+++
@@ -1,8 +0,0 @@
-numeric = int(input('Введите трезначное число: '))
-number0 = numeric % 10
-number1 = numeric % 100 // 10
-number2 = numeric // 100
-sum = number0 + number1 + number2
-product = number0 * number1 * number2
-print(f'Сумма = {sum}')
-print(f'Произведение = {product}')
--- task-6.py
+++
@@ -1,3 +0,0 @@
-number = int(input('Введите номер буквы в английском алфавите от 1 до 26: '))
-letter = chr(number + 96)
-print(f'Это буква {letter}')
