9e530a59be86387ba51a5d1741f6df8e87ad34b4
fighteraa <fighteraa@gmail.com>
2019-02-11 09:32:08 +0300

    Lesson-8

--- task-1.py
+++ task-1.py
@@ -1,35 +1,27 @@
-# 1. Отсортируйте по убыванию методом пузырька одномерный целочисленный массив, заданный
-# случайными числами на промежутке [-100; 100). Выведите на экран исходный и
-# отсортированный массивы. Сортировка должна быть реализована в виде функции. По
-# возможности доработайте алгоритм (сделайте его умнее).
+# 1. Определение количества различных подстрок с использованием хеш-функции. Пусть
+# дана строка S длиной N, состоящая только из маленьких латинских букв. Требуется найти
+# количество различных подстрок в этой строке.
 
 
-import random
+def find_subline_hash(line):
+    len_sub = 1
+    subline_set = set()
+    while len_sub <= len(line):
+        for i in range(len(line)):
+            if i + len_sub <= len(line) and len_sub < len(line):
+                hash_subline = hash(line[i:i + len_sub])
+                if hash_subline in subline_set:
+                    continue
+                else:
+                    subline_set.add(hash_subline)
+                    # print(f'{line[i:i + len_sub]} = {hash_subline}') #Вывод подстроки и ее хеша
+        len_sub += 1
+    return subline_set
 
 
-def bubble_sort(array, flag=True):
-    if not flag:
-        return array
-    flag = False
-    for i in range(len(array) - 1):
-        if array[i] < array[i + 1]:
-            array[i], array[i + 1] = array[i + 1], array[i]
-            flag = True
-    if flag:
-        bubble_sort(array)
-    else:
-        flag = False
-        bubble_sort(array, flag)
-    return array
+line = (input('Введите строку: ')).lower()
+print(line)
+answer_set = find_subline_hash(line)
+print(f'Количество различных подстрок: {len(answer_set)}')
 
 
-size = 10
-MIN_ITEM = -100
-MAX_ITEM = 99
-rand_array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(size)]
-
-# rend_array = [i for i in range(-100, 100)]
-
-random.shuffle(rand_array)
-print(f'Исходный массив: \n{rand_array}')
-print(f'Отсортированный массив: \n{bubble_sort(rand_array)}')
